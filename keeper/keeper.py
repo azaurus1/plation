a38282e2e52762365e007cf04e89b20d7e7217bb
Liam Aikin <liamaikin@gmail.com>
2022-05-18 23:39:46 +1000

    added send close function to keeper

--- keeper/keeper.py
+++ keeper/keeper.py
@@ -1,15 +1,36 @@
-from email.policy import strict
-from tracemalloc import start
+from multiprocessing.connection import wait
 from web3.auto import Web3
 import os, json, time, threading
 
-def handle_opened(event):
-    start_time = time.time()
-    closeTime = start_time + timePeriod
+def sendClose(address):
+    #Build Transaction
+    print(address)
+    txn = PredictionHandler.functions.closePrediction(
+        address
+        ).buildTransaction(
+            {
+                'from':'0x0a29bd68d085CcFa30bDFf2c2ca849D5976a4C9D',
+                'nonce':w3.eth.getTransactionCount('0x0a29bd68d085CcFa30bDFf2c2ca849D5976a4C9D')
+            }
+        )
+    print('txn')
+    print(txn)
+    print('------------------')
+    #Sign transaction with w3.eth.account.sign_transaction()
+    signed_txn = w3.eth.account.sign_transaction(txn,Priv)
+    print('signed txn')
+    print(signed_txn)
+    print('------------------')
+    #Broadcast the transaction with send_raw_transaction()
+    w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
-    openDict[event['args']['_predictionAddress']] = start_time
-    
-    print(openDict)
+
+def handle_opened(event):
+    print(event['args']['_predictionAddress'])
+    predictionAddr = str(event['args']['_predictionAddress'])
+    time.sleep(5)
+    sendClose(predictionAddr)
+
 
 def handle_closed(event):
     print(event)
@@ -17,16 +38,21 @@ def handle_closed(event):
 def log_loop(event_filter, poll_interval):
     while True:
         for event in event_filter.get_new_entries():
+            print('New Prediction Opened')
             handle_opened(event=event)
             time.sleep(poll_interval)
 
 
-#Init 
+#Init
+global Priv 
+global PredictionHandler
+
 Priv = os.getenv('PRIV_KEY')
 Provider = os.getenv('RPC')
 w3 = Web3(Web3.HTTPProvider(Provider))
 openDict = {}
 timePeriod = 86400
+
 
 
 #Contracts Addresses
